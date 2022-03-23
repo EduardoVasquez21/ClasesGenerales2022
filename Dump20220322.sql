63521519ae6b2bd653258db023a2af6a30cfb9fc
EduardoVasquez21 <78975314+EduardoVasquez21@users.noreply.github.com>
2022-03-22 18:28:37 -0600

    DatabaseDump

--- Dump20220322.sql
+++ Dump20220322.sql
@@ -18,6 +18,110 @@ USE `clase`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `estudiante`
+--
+
+DROP TABLE IF EXISTS `estudiante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudiante` (
+  `idEstudiante` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Apellido` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idEstudiante`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudiante`
+--
+
+LOCK TABLES `estudiante` WRITE;
+/*!40000 ALTER TABLE `estudiante` DISABLE KEYS */;
+INSERT INTO `estudiante` VALUES (10,'Ed','Vasquez'),(11,'Edy','Lopez'),(12,'Eduar','Vasquez');
+/*!40000 ALTER TABLE `estudiante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materia`
+--
+
+DROP TABLE IF EXISTS `materia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materia` (
+  `idMateria` int NOT NULL AUTO_INCREMENT,
+  `NombreMateria` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idMateria`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materia`
+--
+
+LOCK TABLES `materia` WRITE;
+/*!40000 ALTER TABLE `materia` DISABLE KEYS */;
+INSERT INTO `materia` VALUES (1,'Progra3');
+/*!40000 ALTER TABLE `materia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `nota`
+--
+
+DROP TABLE IF EXISTS `nota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `nota` (
+  `idNota` int NOT NULL AUTO_INCREMENT,
+  `nota` varchar(45) DEFAULT NULL,
+  `idMateria` int DEFAULT NULL,
+  `idEstudiante` int DEFAULT NULL,
+  PRIMARY KEY (`idNota`),
+  KEY `fk_nota_materia_idx` (`idMateria`),
+  KEY `fk_nota_estudiante1_idx` (`idEstudiante`),
+  CONSTRAINT `fk_nota_estudiante1` FOREIGN KEY (`idEstudiante`) REFERENCES `estudiante` (`idEstudiante`),
+  CONSTRAINT `fk_nota_materia` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`idMateria`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `nota`
+--
+
+LOCK TABLES `nota` WRITE;
+/*!40000 ALTER TABLE `nota` DISABLE KEYS */;
+INSERT INTO `nota` VALUES (1,'9',1,10);
+/*!40000 ALTER TABLE `nota` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idusuario` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (1,'Rodigo','0ffacd815347a9b6294a712e3d87ed7b'),(2,'javier','a14f8a540e78dae706d255750010a0f8'),(3,'Carlos','609c3dc097522eacf0e12ceaf55a916f'),(4,'Adrian','4d186321c1a7f0f354b297e8914ab240'),(5,'Eduardo','Vk7'),(6,'PrUser','PPASSWORD'),(8,'Carloos','235d550ad895a7e483db8acd4cc302ee'),(9,'eddu','1234'),(10,'Eduardo','Vkaiido'),(11,'Eduardo','61ab6a926e43e252b5274feae8190257'),(12,'Prueba','2fe04e524ba40505a82e03a2819429cc'),(13,'pruebita','99f69c4af9920725c0b75f2809edc96f'),(14,'pruebota','9dd87adb677a9b4632ec515ed811ff9c'),(15,'hola','hola'),(16,'Puerba','8f936c6e527214d26614f49cb61d391b'),(17,'Juan','perez'),(18,'Manuel','540fdbdc9912ae5d8ed5fb0a50bc3578'),(19,'Adrian','solis');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'clase'
 --
 /*!50003 DROP PROCEDURE IF EXISTS `SPS_LOG` */;
@@ -146,4 +250,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-22 17:41:12
+-- Dump completed on 2022-03-22 18:25:39
